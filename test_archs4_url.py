39c5ae76485c6d30f57198317ad87b9dcac65309
minggangli1030 <minggangli@berkeley.edu>
2026-04-20 10:54:42 -0700

    Test v11 archs4 remote access

--- test_archs4_url.py
+++ test_archs4_url.py
@@ -1,21 +1,26 @@
+import archs4py as a4
 import s3fs
+import h5py
 
-# List contents of the mssm-seq-matrix bucket to find correct file paths
-print("Listing mssm-seq-matrix bucket...")
+HUMAN_URL = "https://s3.amazonaws.com/mssm-seq-matrix/human_matrix_v11.h5"
+MOUSE_URL = "https://s3.amazonaws.com/mssm-seq-matrix/mouse_matrix_v11.h5"
+
+# Test rand_remote
+print("Testing rand_remote with human_matrix_v11.h5...")
+try:
+    df = a4.data.rand_remote(HUMAN_URL, 3, remove_sc=True)
+    print("  WORKS! Shape:", df.shape)
+    print("  First genes:", df.index[:5].tolist())
+except Exception as e:
+    print("  FAILED:", e)
+
+# Check H5 structure (preprocessing.py needs specific keys)
+print("\nChecking H5 structure of human_matrix_v11.h5...")
 try:
     s3 = s3fs.S3FileSystem(anon=True)
-    files = s3.ls("mssm-seq-matrix/")
-    for f in files:
-        print(" ", f)
+    with h5py.File(s3.open("mssm-seq-matrix/human_matrix_v11.h5", "rb"), "r") as f:
+        def print_keys(name, obj):
+            print(" ", name)
+        f.visititems(print_keys)
 except Exception as e:
-    print("mssm-seq-matrix failed:", e)
-
-# Also try maayanlab-public bucket
-print("\nListing maayanlab-public bucket...")
-try:
-    s3 = s3fs.S3FileSystem(anon=True)
-    files = s3.ls("maayanlab-public/")
-    for f in files:
-        print(" ", f)
-except Exception as e:
-    print("maayanlab-public failed:", e)
+    print("  FAILED:", e)
